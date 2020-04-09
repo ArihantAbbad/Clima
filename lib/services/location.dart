4d5c152d652e2ee694a3fbe3f5b79b5c4aa52b0e
Arihant Abbad <54006224+ArihantAbbad@users.noreply.github.com>
2020-04-09 19:21:08 +0530

    Add files via upload

---
+++ lib/services/location.dart
@@ -0,0 +1,18 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double longitude;
+  double latitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+
+      longitude = position.longitude;
+      latitude = position.latitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
---
+++ lib/services/weather.dart
@@ -0,0 +1,57 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = 'f5e187d34563f566b945e501686eaca9';
+
+class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    networkHelpeer networkhelper = networkHelpeer(
+        'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey');
+
+    var weatherData = await networkhelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    networkHelpeer networkhelper = networkHelpeer(
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey');
+
+    var weatherData = await networkhelper.getData();
+    return weatherData;
+  }
+
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return '🌩';
+    } else if (condition < 400) {
+      return '🌧';
+    } else if (condition < 600) {
+      return '☔️';
+    } else if (condition < 700) {
+      return '☃️';
+    } else if (condition < 800) {
+      return '🌫';
+    } else if (condition == 800) {
+      return '☀️';
+    } else if (condition <= 804) {
+      return '☁️';
+    } else {
+      return '🤷‍';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
